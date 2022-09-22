20551b683a100d64b7c5994fd871ffe400ecf7e4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-09-22 14:30:20 -0400

    set custom style rules, and static text in constans folder

---
+++ src/constants/index.js
@@ -0,0 +1,147 @@
+import {
+  FaFacebookSquare as facebook,
+  FaInstagramSquare as instagram,
+  FaLinkedin as linkedin,
+  FaWhatsappSquare as whatsapp,
+  FaStar as star,
+} from "react-icons/fa";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About Me",
+  },
+  {
+    id: "news",
+    title: "Did you know?",
+  },
+  {
+    id: "comments",
+    title: "Comments",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const comments = [
+  {
+    id: "comment-1",
+    icon: star,
+    title: "Thank you.",
+    content:
+      "very useful. i liked and enjoyed the journey of learning in these five weeks. the instructor is very clear and taught very interestingly. Thanks to her. she looked poised and cheerful and professional",
+  },
+  {
+    id: "feature-2",
+    icon: star,
+    title: "Anara made english easy to understand. ",
+    content:
+      "I've benefited a lot by pursuing this course. Small pronunciation and word stress has been cleared. I would suggest non-English speakers to take up this course to enhance their English language skill.",
+  },
+  {
+    id: "feature-3",
+    icon: star,
+    title: "She truly care about her students.",
+    content:
+      "Very great program for ESL learner at intermediate level. I am looking for something at higher level. However, I found your course was very interesting. I have learned new communication skills for the very first time. Something that I thought I have already knew it. I will introduce your class to my international friends. Wish you all the best and your succeed in this program.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: whatsapp,
+    link: "https://wa.me/14042457101",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
