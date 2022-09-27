fbf3c764ed42a6d8ec5f32657d48d0b29bafad5a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-09-27 09:38:10 -0400

    Hero: mobile view finish

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -4,23 +4,17 @@ import { FiArrowUpRight } from "react-icons/fi";
 function GetStarted() {
   return (
     <div
-      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      className={`${styles.flexCenter} rounded bg-teal-600 cursor-pointer px-10 py-2 my-4`}
     >
-      <div
-        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
-      >
+      <div>
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium text-[18px] leading-[23px]">
-            <span className="text-info">Get</span>
+          <p className="font-poppins font-medium text-[17px] ">
+            <span className="text-info">GET STARTED</span>
           </p>
-          <span className="text-[27px] ml-[6px]">
-            <FiArrowUpRight />
+          <span className="text-[25px] ml-[6px]">
+            <FiArrowUpRight color="white" />
           </span>
         </div>
-
-        <p className="font-poppins font-medium text-[18px] leading-[23px]">
-          <span className="text-info">Started</span>
-        </p>
       </div>
     </div>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,8 +20,8 @@ function Hero() {
             </p>
           </div>
 
-          <div className="flex flex-row justify-between items-center w-full">
-            <h1 className="flex-1 font-poppins font-semibold ss:text:[72px] text-[52px] text-white leading-[75px] ss:leading-[100px]">
+          <div className="flex flex-row  justify-between text-center sm:text-left items-center w-full">
+            <h1 className="flex-1 font-poppins font-semibold ss:text:[72px] text-[52px] text-white leading-[65px] ss:leading-[75px]">
               IT'S TIME <br className="hidden sm:block " /> TO LEARN <br />
               <span className="text-info text-[70px]  ">ENGLISH</span>
             </h1>
@@ -35,16 +35,16 @@ function Hero() {
           <img
             src="./hero-2.jpg"
             alt="learn-english"
-            className="w-[70%] h-[82%]  "
+            className="w-[70%] h-[82%]  border-4 border-amber-200 border-y-amber-400 rounded"
           />
         </div>
       </div>
-      <p className="text-white px-16">
-        Knowing English increases your chances of getting a good job in a
-        multinational company within your home country or for finding work
-        abroad. It's also the language of international communication, the media
-        and the internet, so learning English is important for socialising and
-        entertainment as well as work!
+      <p className="text-white px-16 text-sm text-center">
+        It is the language of science, aviation, computers, diplomacy and
+        tourism. Last but not least, it is the language of international
+        communication, the media and the internet. Whether it is for
+        professional or personal reasons, understanding the importance of
+        English will help you reach your goals.
       </p>
       <div className={`${styles.flexCenter} ss:hidden`}>
         <GetStarted />
