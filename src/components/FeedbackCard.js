cf01c63502abe2c252ff531c6bd7a7126d72a5b1
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-09-29 09:34:48 -0400

    Testimonials star icon

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -1,16 +1,17 @@
-import { FaQuoteRight, FaStar } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
+
 const FeedbackCard = ({ title, content }) => (
-  <div className="flex justify-between flex-col px-5 py-6 rounded-[20px] max-w-[300px] sm:mr-6 mr-0 my-5 bg-discount-gradient ">
+  <div className="flex justify-between flex-col px-5 py-6 rounded-[20px] max-w-[300px] sm:mr-6  my-5 bg-discount-gradient ">
     <FaQuoteRight className="w-[32px] h-[17px] object-contain text-teal-600" />
     <p className="font-poppins font-normal text-[15px] leading-[28px] text-dimWhite my-4 ">
       {content}
     </p>
     <div className="flex flex-row justify-center ">
-      <FaStar className="text-white text-[30px] m-0.5" />
-      <FaStar className="text-white text-[30px] m-0.5" />
-      <FaStar className="text-white text-[30px] m-0.5" />
-      <FaStar className="text-white text-[30px] m-0.5" />
-      <FaStar className="text-white text-[30px] m-0.5" />
+      <div className="text-[30px] m-0.5">⭐️</div>
+      <div className="text-[30px] m-0.5">⭐️</div>
+      <div className="text-[30px] m-0.5">⭐️</div>
+      <div className="text-[30px] m-0.5">⭐️</div>
+      <div className="text-[30px] m-0.5">⭐️</div>
     </div>
   </div>
 );
