992836df665c8c8f1d8dc98fa6e3ef61f2dfbd30
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-10-03 20:26:53 -0400

    Hero: Send a message btn functionallity

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -3,11 +3,11 @@ import { FaRegEnvelope } from "react-icons/fa";
 
 function GetStarted() {
   return (
-    <div
-      className={`${styles.flexCenter} rounded bg-teal-600 cursor-pointer px-10 py-2 my-4`}
+    <button
+      className={`${styles.flexCenter} rounded bg-teal-600 hover:bg-teal-700 cursor-pointer px-10 py-2 my-4`}
     >
-      <div>
-        <div className={`${styles.flexStart} flex-row`}>
+      <a href="mailto:dinara.idrissova@list.ru">
+        <div className={`${styles.flexStart} flex-row `}>
           <p className="font-poppins font-medium text-[17px] ">
             <span className="text-info uppercase tracking-wider font-semibold">
               отправить сообщение
@@ -17,8 +17,8 @@ function GetStarted() {
             <FaRegEnvelope className="ml-2 text-info" />
           </span>
         </div>
-      </div>
-    </div>
+      </a>
+    </button>
   );
 }
 
