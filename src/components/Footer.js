2706f93a5265cab667c8b9f3df11dde54bf02d6e
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-10-04 11:42:32 -0400

    Footer: new structure and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,54 @@
-import { socialMedia } from "../constants";
+import { FaPhone, FaRegEnvelope, FaWhatsappSquare } from "react-icons/fa";
 
 function Footer() {
   return (
     <section
-      id="footer"
+      id="contact"
       className="sm:w-full w-[70vw] flex flex-col justify-center"
     >
-      <div className="shadow-inner h-[18px]  w-[200vw] -ml-[200px] "></div>
+      <div className="shadow-inner h-[22px]  w-[200vw] -ml-[200px] "></div>
       <h2
-        className=" text-center mt-4 font-bold text-white
+        className=" text-center my-4 font-bold text-white uppercase  text-[30px] tracking-widest 
       "
       >
-        GET CONNECTED
+        Контактные данные
       </h2>
 
-      <div className=" flex justify-center  ">
-        <div className=" flex">
-          {socialMedia.map((footerLink) => (
-            <div key={footerLink.id} className=" my-2 min-w-[70px] flex ">
-              <a href={footerLink.link}>
-                <footerLink.icon className="text-[40px] text-white hover:bg-white hover:text-primary  shadow-md border rounded  " />
-              </a>
+      <div className=" flex flex-col sm:flex-row justify-around h-full items-center ">
+        <div className="mb-6">
+          <a
+            href="tel:+YOURNUMBERHERE"
+            className="flex flex-col text-dimWhite "
+          >
+            <div className="flex gap-3 items-center justify-center mb-2">
+              <FaPhone /> телефон
             </div>
-          ))}
+            <p className="underline text-[#0000FF] ">+777 555 55 55</p>
+          </a>
+        </div>
+
+        <div className="mb-6">
+          <a href="https://wa.me/14042457101">
+            <div className="flex gap-3 items-center justify-center text-dimWhite mb-2">
+              <FaWhatsappSquare className="text-[24px]" />
+              WhatsApp
+            </div>
+            <p className="underline text-[#0000FF] ">+777 555 55 55</p>
+          </a>
+        </div>
+        <div className="mb-6">
+          <a href="mailto:dinara.idrissova@list.ru">
+            <div className="flex gap-3 items-center justify-center text-dimWhite mb-2">
+              <FaRegEnvelope className="text-[24px]" /> электронная почта
+            </div>
+            <p className="underline text-[#0000FF] text-center">
+              example@gmail.com
+            </p>
+          </a>
         </div>
       </div>
 
-      <p className="text-[12px] p-3 text-center text-[#3B3B38] ">
+      <p className="text-[12px] p-3 text-center text-[#3B3B38] mt-6">
         Copyright &copy; 2022. All rights reserved. <br /> Website created by
         <a
           className="underline pl-1"
