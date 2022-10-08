b9703b7a4b073090dab6fcae2c183e0873ec7b89
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-10-07 20:36:09 -0400

    resizing for Stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,8 +3,8 @@ import styles from "../style";
 
 const Stats = () => (
   <>
-    <div className="  text-center my-6  font-bold bg-white  rounded-tl-xl rounded-br-xl -skew-x-12 bg-opacity-50 mx-20 py-2">
-      <h1 className="text-gradient text-2xl ">
+    <div className=" text-center my-4  font-bold bg-white  rounded-tl-xl rounded-br-xl -skew-x-12 bg-opacity-50 mx-20 p-4">
+      <h1 className="text-gradient text-md sm:text-2xl ">
         Почему так важно изучать английский?
       </h1>
     </div>
@@ -14,12 +14,12 @@ const Stats = () => (
       {stats.map((stat) => (
         <div
           key={stat.id}
-          className={`flex-1 flex justify-start items-center flex-row  sm:m-1  border-2  border-teal-600 h-12  sm:h-12  sm:p-2 px-2  rounded shadow-lg`}
+          className={`flex-1 flex justify-start items-center flex-row  sm:m-1  border-2  border-teal-600 h-12  sm:h-12  sm:p-2 px-2 py-6 sm:py-8 rounded shadow-lg`}
         >
           <h4 className="font-poppins font-semibold sm:text-[17px] text-[13px]  text-amber-600 italic pr-2">
             {stat.value}
           </h4>
-          <p className="font-poppins font-normal sm:text-[12px] text-[9px] uppercase  text-white">
+          <p className="font-poppins font-normal sm:text-[10px] text-[8px] uppercase  text-white">
             {stat.title}
           </p>
         </div>
